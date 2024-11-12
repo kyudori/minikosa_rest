c381a8bd496e8480b1b76653b4cb650b746c8472
Han KyuHyun <khh5345@naver.com>
2024-11-12 12:28:57 +0900

    feat: session 추가
    Signed-off-by: Han KyuHyun <khh5345@naver.com>

--- src/main/java/com/kosa/mini/api/controller/member/LoginApiController.java
+++ src/main/java/com/kosa/mini/api/controller/member/LoginApiController.java
@@ -3,6 +3,7 @@ package com.kosa.mini.api.controller.member;
 import com.kosa.mini.api.domain.member.LoginDTO;
 import com.kosa.mini.api.domain.member.UserSessionDTO;
 import com.kosa.mini.api.service.member.LoginService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,9 @@ public class LoginApiController {
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public UserSessionDTO login(@RequestBody LoginDTO loginDTO) {
-        return loginService.authenticate(loginDTO);
+    public UserSessionDTO login(@RequestBody LoginDTO loginDTO, HttpSession session) {
+        UserSessionDTO userSession = loginService.authenticate(loginDTO);
+        session.setAttribute("user", userSession); // 세션에 사용자 정보 저장
+        return userSession;
     }
 }
