d51ce0a192634a32b6f0f80f59a2d6d13c3f3fd5
ksks1234 <logging2021@gmail.com>
2024-11-15 17:27:03 +0900

    rename: home에서 사용하는 dto 적용

--- src/main/java/com/kosa/mini/api/service/home/HomeApiService.java
+++ src/main/java/com/kosa/mini/api/service/home/HomeApiService.java
@@ -1,10 +1,10 @@
 package com.kosa.mini.api.service.home;
 
-import com.kosa.mini.api.dto.home.StoreDTO;
+import com.kosa.mini.api.dto.home.HomeStoreDTO;
 
 import java.util.List;
 
 public interface HomeApiService {
 
-    public List<StoreDTO> home();
+    public List<HomeStoreDTO> home();
 }
